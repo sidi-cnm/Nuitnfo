2ff7b3ba802beb2359674f982ae0d47d63d5cd95
sidi-cnm <sdeiebeidy@gmail.com>
2025-11-18 22:27:13 +0000

    second commit

---
+++ app/home/page.tsx
@@ -0,0 +1,10 @@
+
+
+
+import Homecomponent from '../../components/Homecomponent';
+
+export default  function Home() {
+    return <div>
+        <Homecomponent />
+    </div>;
+}
---
+++ components/Homecomponent.tsx
@@ -0,0 +1,5 @@
+
+
+export default function Homecomponent() {
+    return <div className="bg-red">Home Components</div>;
+}
